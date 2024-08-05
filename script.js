7883a366b632848206863fee613651abe544ece7
Magaly-N <magalynou@hotmail.fr>
2024-08-05 16:20:14 +0200

    Opening and closing items

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 $(function () {
 
-    // Other way to declare variables
-    var $mainMenuItems = $('#main-menu ul').children('li'),
-        totalMainMenuItems = $mainMenuItems.lenght,
-        openedIndex = -1,
+    // Declare variables
+    var $mainMenuItems = $('#main-menu ul').children('li'), // Select all 'li' elements that are children of '#main-menu ul'
+        totalMainMenuItems = $mainMenuItems.length, // Get the number of main menu items
+        openedIndex = -1, // Variable to keep track of the opened index, initialized to -1 (no item opened)
 
+        // Initialize the menu
         init = function () {
 
+            // Add click event handler to children with class '.img' within main menu items
             $mainMenuItems.children('.img').click(function () {
+                var newIndex = $(this).parent().index(), // Get the index of the clicked item
+                    $item = $mainMenuItems.eq(newIndex); // Get the clicked item based on the index
 
-                var newIndex = $(this).parent().index(),
-                    $item = $mainMenuItems.eq(newIndex),
-                    $colorImage = $item.find('.color');
-
-                $colorImage.animate({ left: "0px" }, 250);
-                $item.animate({ width: "420px" }, 250);
-
-                openedIndex = newIndex;
+                // Check if the clicked item is already open
+                if (openedIndex === newIndex) {
+                    // If it's open, animate to close the item
+                    animateItem($item, false, 250);
+                    openedIndex = -1; // Reset the openedIndex since no item will be open
+                } else {
+                    // If the clicked item is different from the opened one
+                    if (validIndex(newIndex)) { // Validate the new index
+                        // Animate to close the currently opened item
+                        animateItem($mainMenuItems.eq(openedIndex), false, 250);
+                        openedIndex = newIndex; // Update the openedIndex to the new one
+                        // Animate to open the clicked item
+                        animateItem($item, true, 250);
+                    }
+                }
             });
         };
+
+    // Function to validate if the index is within the range of menu items
+    validIndex = function (indexToCheck) {
+        return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
+    }
+
+    // Function to animate menu items
+    animateItem = function ($item, toOpen, speed) {
+        var $colorImage = $item.find('.color'); // Find the '.color' element within the item
+        // Set animation parameters based on whether the item is being opened or closed
+        itemParam = toOpen ? { width: "420px" } : { width: "140px" };
+        colorImageParam = toOpen ? { left: "0px" } : { left: "140px" };
+        // Animate the '.color' element
+        $colorImage.animate(colorImageParam, speed);
+        // Animate the item itself
+        $item.animate(itemParam, speed);
+    };
+
+    // Call the init function to set up the menu
     init();
+
 });
